a8d63fe6be47572a6f86b007957964da2e2a4124
yruns <yruns.sh@qq.com>
2023-10-26 04:22:57 +0800

    Initial commit

---
+++ models/pointnet.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from tnet import TNet, conv_batch, fc_batch
+
+class PointNet(nn.Module):
+    """
+    PointNet
+    """
+    def __init__(self, k, mode="cls", device="cpu"):
+        """
+        :param k: 类别个数
+        """
+        super(PointNet, self).__init__()
+        self.k = k
+        self.mode = mode
+        self.device = device
+
+        self.input_transform = TNet(conv_channels=[(3, 64), (64, 128), (128, 1024)], device=device)
+        self.feature_transform = TNet(conv_channels=[(64, 64), (64, 128), (128, 1024)], device=device)
+
+        self.conv1 = conv_batch(3, 64)
+        self.conv2 = conv_batch(64, 128)
+        self.conv3 = nn.Sequential(
+            nn.Conv1d(128, 1024, kernel_size=1, stride=1),
+            nn.BatchNorm1d(num_features=1024)
+        )
+
+        assert mode in ["cls", "seg"], "mode just only be 'cls' or 'seg'"
+
+        if mode == "cls":
+            # use for classification
+            self.classifier = nn.Sequential(
+                nn.Linear(1024, 512),
+                nn.BatchNorm1d(512),
+                nn.Dropout(0.3),
+                nn.ReLU(),
+                nn.Linear(512, 256),
+                nn.BatchNorm1d(256),
+                nn.Dropout(0.3),
+                nn.ReLU(),
+                nn.Linear(256, k)
+            )
+        elif mode == "seg":
+            # use for segmentation
+            self.seg_conv = nn.Sequential(
+                conv_batch(1088, 512),
+                conv_batch(512, 256),
+                conv_batch(256, 128),
+                nn.Conv1d(128, k, 1, 1)
+            )
+
+    def forward(self, x):
+        """
+        :param x: shape: [-1, d, n] (d=3)
+        :return:  shape:
+        """
+        n = x.shape[2]
+
+        # first T-net compute
+        trans = self.input_transform(x)  # [-1, d, d]
+        x = x.permute(0, 2, 1)  # [-1, d, n] -> [-1, n, d]
+        x = torch.bmm(x, trans) # [-1, n, d] @ [-1, d, d] -> [-1, n, d]
+        x = x.permute(0, 2, 1)  # [-1, d, n]
+
+        x = self.conv1(x) # [-1, 64, n]
+
+        # second T-net compute
+        trans = self.feature_transform(x)
+        x = x.permute(0, 2, 1)
+        x = torch.bmm(x, trans)
+        points_feature = x.permute(0, 2, 1)  # [-1, 64, n]
+
+        x = self.conv2(points_feature)
+        x = self.conv3(x)   # [-1, 1024, n]
+
+        global_feature = torch.max(x, dim=2, keepdim=True)[0].view(-1, 1024) # [-1, 1024]
+
+        if self.mode == "cls":
+            logits = self.classifier(global_feature)
+            return logits  # [-1, k]
+        elif self.mode == "seg":
+            global_feature = global_feature.unsqueeze(-1).repeat(1, 1, n)  # [-1, 1024, n]
+            points_feature = torch.cat((points_feature, global_feature), dim=1)
+            logits = self.seg_conv(points_feature)
+
+            logits = logits.permute(0, 2, 1).contiguous()
+            return logits  # [-1, n, k]
+
+
+if __name__ == '__main__':
+    x = torch.randn(4, 16, 3).permute(0, 2, 1)
+
+    net = PointNet(k=10, mode="cls", device="cpu")
+    y = net(x)
+    print(y.shape)
+
+
+
+        
---
+++ models/tnet.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+
+def conv_batch(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv1d(in_channels, out_channels, kernel_size=1, stride=1),
+        nn.BatchNorm1d(num_features=out_channels),
+        nn.ReLU()
+    )
+
+def fc_batch(input_dim, output_dim):
+    return nn.Sequential(
+        nn.Linear(input_dim, output_dim),
+        nn.BatchNorm1d(output_dim),
+        nn.ReLU()
+    )
+
+class TNet(nn.Module):
+    """
+    Input transform module for PointNet
+    """
+    
+    def __init__(self, conv_channels, device="cpu"):
+        """
+        :param conv_channels: 多层卷积维度
+        :param device:
+        """
+        super(TNet, self).__init__()
+        self.d = conv_channels[0][0]
+        self.device = device
+
+        self.conv1 = conv_batch(*conv_channels[0])
+        self.conv2 = conv_batch(*conv_channels[1])
+        self.conv3 = conv_batch(*conv_channels[2])
+
+        self.fc1 = fc_batch(1024, 512)
+        self.fc2 = fc_batch(512, 256)
+        self.weight = nn.Linear(256, self.d * self.d, bias=False)
+
+    def forward(self, x):
+        """
+        :param x: shape: [-1, d, n]
+        :return:  shape: [-1, d, d]
+        """
+        batch_size = x.shape[0]
+
+        x = self.conv1(x)   # [-1, 64, n]
+        x = self.conv2(x)   # [-1, 128, n]
+        x = self.conv3(x)   # [-1, 1024, n]
+
+        x = torch.max(x, dim=2, keepdim=True)[0]  # [-1, 1024, 1]
+        x = x.view(-1, 1024)  # [-1, 1024]
+
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.weight(x)   # [-1, d*d]
+
+        iden = torch.eye(self.d, requires_grad=True).flatten().view(1, self.d * self.d)\
+            .repeat(batch_size, 1).to(self.device)  # iden.shape: [-1, d*d]
+
+        x += iden  # [-1, d*d]
+        x = x.view(-1, self.d, self.d)  # [-1, d, d]
+
+        return x
+
+
+
+if __name__ == '__main__':
+    x = torch.randn(4, 16, 3).permute(0, 2, 1)
+
+    Tnet = TNet(conv_channels=[(3, 64), (64, 128), (128, 1024)])
+    y = Tnet(x)
